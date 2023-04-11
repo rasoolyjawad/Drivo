c8ef9f8c1fb8da6df5b723f42a37ab21e1947a6c
rasoolyjawad <rasoolyjawad16@gmail.com>
2023-04-11 18:06:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// "use strict";
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todoUL = document.getElementById("todo");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodo();
+});
+function updateLS() {
+  todosEl = document.querySelectorAll("li");
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+    });
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function addTodo(todo) {
+  let todoText = input.value + ` zl`;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+  const todoEl = document.createElement("li");
+  todoEl.innerText = todoText;
+  todoEl.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    todoEl.remove();
+    updateLS();
+  });
+
+  todoUL.appendChild(todoEl);
+
+  input.value = "";
+  updateLS();
+}
